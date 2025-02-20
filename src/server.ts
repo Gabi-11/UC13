10beee6ee3c87fc1c33dbdb105522989071ae49c
Gabriel <aulinhasgabriel@gmail.com>
2025-02-20 20:01:08 -0300

    Oi

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,14 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const PORT: number = 3000;
 
-let usuarios: object[] = [
-  {"nome": "Gabriel"}
-]
-
-let sobre: object[] = [
-  {"nome completo": "Gabriel de Graaf", "idade": "18", "descricao": "Sou estudante do curso de Desenvolvimento de Sistemas no Senac"}
-]
 app.use(express.json());
 
-const porteiroMiddleware = (req: Request, res: Response, next: Function) => {
+const logger = (req: Request, res: Response, next: Function) => {
+  const data: String = new Date().toISOString();
   console.log(`📢 Requisição recebida em: ${req.url}`);
   next(); // Permite a requisição continuar para a rota
 };
 
-app.use(porteiroMiddleware);
 
 // 🔹 Rota GET (Buscar dados)
 app.get('/usuarios', (req: Request, res: Response) => {
@@ -40,15 +33,12 @@ app.post('/usuarios', (req: Request, res: Response) => {
 });
 
 app.post('/sobre', (req: Request, res: Response) => {
-  const { nomecompleto } = req.body as {nomecompleto: string};
-  if (!nomecompleto) {
-    res.status(400).json({ mensagem: 'Nome é obrigatório!' });
-  } else{
-    res.status(201).json({ mensagem: `Usuário ${nomecompleto} criado com sucesso!` });
-    res.status(201).json({ mensagem: `Idade Usuário ${nomecompleto}` });
-    res.status(201).json({ mensagem: `Descrição: ${nomecompleto}` });
-  }
-  }
-);
+  res.status(200).json({
+    nome: "Gabriel",
+    idade: 18,
+    descricao: "Aluno do Curso"
+  })
+
+});
 
 app.listen(PORT, () => console.log(`🔥 Servidor rodando em http://localhost:${PORT}`));
